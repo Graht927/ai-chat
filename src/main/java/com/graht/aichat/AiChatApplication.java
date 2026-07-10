b3563ccfd4224b1f1bfae8dd5e465f6c031db437
graht <430702238@qq.com>
2026-07-10 19:16:02 +0800

    chore: initialize Spring Boot Project

---
+++ src/main/java/com/graht/aichat/AiChatApplication.java
@@ -0,0 +1,14 @@
+package com.graht.aichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatApplication.class, args);
+		System.out.println("Started AiChatApplication ...");
+	}
+
+}
