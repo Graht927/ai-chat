83ca5abd2581b5ba8721eb1d6437bf99324f7200
graht <430702238@qq.com>
2026-07-11 20:40:00 +0800

    feat: add vlidation architecture

--- src/main/java/com/graht/aichat/common/ResultCode.java
+++ src/main/java/com/graht/aichat/common/ResultCode.java
@@ -8,6 +8,8 @@ package com.graht.aichat.common;
 public enum ResultCode {
     SUCCESS(200,"success"),
     SYSTEM_ERROR(500,"server fail"),
+    INVALID_PARAMETER(600,"invalid parameter"),
+    PARAM_VALIDATION_FAILED(601,"param validation failed"),
     ;
     private final int code;
     private final String message;
--- src/main/java/com/graht/aichat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/graht/aichat/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.graht.aichat.exception;
 
 import com.graht.aichat.common.AIChatResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 /**
  * @author GRAHT
@@ -13,6 +15,18 @@ public class GlobalExceptionHandler{
     public AIChatResult<String> handle(Exception e){
         //TODO change logger
         e.printStackTrace();
+        return AIChatResult.fail(e.getMessage());
+    }
+    @ExceptionHandler
+    public AIChatResult<String> handle(BusinessException e){
+        return AIChatResult.fail(e.getResultCode().getMessage());
+    }
+    @ExceptionHandler
+    public AIChatResult<String> handle(HandlerMethodValidationException e){
+        return AIChatResult.fail(e.getMessage());
+    }
+    @ExceptionHandler
+    public AIChatResult<String> handle(MethodArgumentNotValidException e){
         return AIChatResult.fail(e.getMessage());
     }
 }
